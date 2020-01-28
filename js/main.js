77a21c7f82f76d858a7c2cb1c84c380e2614403b
Puredevs <azhar@puredevs.com>
2020-01-28 12:20:33 +0600

    stater new update 2020

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function($, window, document,) {
+    "use strict";
+    var $win = $(window);
+    var $doc = $(document);
+    var $body = $('body');
+
+    /*--
+    Menu Stick
+    -----------------------------------*/
+   
+    
+    
+    
+})(jQuery);
